a7ef9c589d7d72ea28635668c3a9603f82943679
Akbar Haryadi <akbarharyadi777@gmail.com>
2017-04-09 21:22:42 +0700

    model wilayah

--- app/models/wilayah.rb
+++ app/models/wilayah.rb
@@ -4,6 +4,6 @@ class Wilayah < ApplicationRecord
     # friendly_id :nama_wilayah, use: :slugged
 
     #Validasi nama wilayah
-    validates :nama_wilayah, :jenis_wilayah, :pemimpin, :ibu_kota, :nama_badan, :alamat_badan, :telp_badan, presence: { message: " tidak boleh kosong" }
+    validates :nama_wilayah, :jenis_wilayah, :pemimpin, :ibu_kota, :nama_badan, :alamat_badan, :singkatan, presence: true
 
 end
